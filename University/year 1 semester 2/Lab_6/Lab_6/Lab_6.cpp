31baf77a70cadc3391bc6e33ceae7f6be93e02ea
khivus <aplls5643@yandex.ru>
2022-05-30 20:32:36 +0300

    Lab_6 v0.0.1 creation

---
+++ University/year 1 semester 2/Lab_6/Lab_6/Lab_6.cpp
@@ -0,0 +1,16 @@
+// Classes
+// Version 0.0.1
+// 2022
+// Kharin Aleksey
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Lmao\n";
+
+    cout << "\nPress any key to continiue...\n";
+    while (!_kbhit());
+}
